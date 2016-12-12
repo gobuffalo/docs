b2458bdf5c4f4d1b9456d4b9ef76a2ada1dd0972
Mark Bates <mark@markbates.com>
2016-12-12 13:35:20 -0500

    added newrelic

--- actions/app.go
+++ actions/app.go
@@ -1,6 +1,7 @@
 package actions
 
 import (
+	"log"
 	"net/http"
 	"os"
 
@@ -8,6 +9,7 @@ import (
 	"github.com/markbates/buffalo/middleware"
 	"github.com/markbates/gobuffalo/models"
 	"github.com/markbates/going/defaults"
+	newrelic "github.com/newrelic/go-agent"
 )
 
 var ENV = defaults.String(os.Getenv("GO_ENV"), "development")
@@ -19,6 +21,22 @@ func App() http.Handler {
 	a := buffalo.Automatic(buffalo.Options{
 		Env: ENV,
 	})
+
+	nrkey := os.Getenv("NEW_RELIC_LICENSE_KEY")
+	if nrkey != "" {
+		config := newrelic.NewConfig("gobuffalo.io", nrkey)
+		app, err := newrelic.NewApplication(config)
+		if err != nil {
+			log.Fatal(err)
+		}
+		a.Use(func(next buffalo.Handler) buffalo.Handler {
+			return func(c buffalo.Context) error {
+				tx := app.StartTransaction(c.Request().URL.String(), c.Response(), c.Request())
+				defer tx.End()
+				return next(c)
+			}
+		})
+	}
 
 	a.Use(middleware.PopTransaction(models.DB))
 	a.ServeFiles("/assets", assetsPath())

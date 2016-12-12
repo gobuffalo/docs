50c0fe618920092bd7e8775cc0edfa60a77ec69d
Mark Bates <mark@markbates.com>
2016-12-12 12:05:39 -0500

    added glide

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/markbates/gobuffalo/actions"
+	"github.com/markbates/going/defaults"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":3000", actions.App()))
+	port := defaults.String(os.Getenv("PORT"), "3000")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), actions.App()))
 }

049692d35e64d5dc794eb721004054966e1d9e00
Mark Bates <mark@markbates.com>
2017-01-04 16:10:25 -0500

    clean up the bin artifact

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@ set +x
 
 git checkout -b build
 GOOS=linux buffalo build -z
+git add .
 git commit -a -m "binary commit"
 git push heroku build:master
 git checkout master
 git branch -d build
+rm -rf bin/

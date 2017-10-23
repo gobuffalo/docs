d7315bcc188c6fbec4386e4e92d32af3a5a12da2
Stanislas Michalak <stanislas.michalak@gmail.com>
2017-10-23 22:08:47 +0200

    Improve the New Project page
    * Add more details & pictures
    * Add doc about the custom dev port
    * Rewrite Note helper to be used as a block
    * Add a max-height to code snippets

---
+++ actions/helpers/note.go
@@ -0,0 +1,35 @@
+package helpers
+
+import (
+	"html/template"
+	"fmt"
+
+	"github.com/gobuffalo/plush"
+	"github.com/gobuffalo/tags"
+	"github.com/pkg/errors"
+	"github.com/shurcooL/github_flavored_markdown"
+)
+
+// Note is a template block helper to insert a remark.
+func Note(help plush.HelperContext) (template.HTML, error) {
+	if !help.HasBlock() {
+		return "", nil
+	}
+	s, err := help.Block()
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+
+	b := github_flavored_markdown.Markdown([]byte(s))
+
+	// Generate info div
+	// Strip the first <p> from Markdown result to insert the icon.
+	t := tags.New("div", tags.Options{
+		"class": "info",
+		"body": fmt.Sprintf(note, b[3:]),
+	})
+
+	return t.HTML(), nil
+}
+
+const note = `<p><i class="fa fa-info-circle" aria-hidden="true"></i>%s`

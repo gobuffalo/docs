cecd91e9d0debd5b013e9f39bc5d2988c6fb75ff
Mark Bates <mark@markbates.com>
2017-11-06 16:47:52 -0500

    fix, hopefully, broken heroku

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"log"
 
-	"github.com/gobuffalo/envy"
 	"github.com/gobuffalo/gobuffalo/actions"
 )
 
 func main() {
-	port := envy.Get("PORT", "3000")
-	log.Fatal(actions.App().Start(port))
+	log.Fatal(actions.App().Serve())
 }

40df1930485a9742d1d040854b8a82cc3baa6886
Mark Bates <mark@markbates.com>
2017-11-06 13:49:52 -0500

    updated to 0.10.0

--- actions/app.go
+++ actions/app.go
@@ -30,7 +30,7 @@ func App() *buffalo.App {
 
 		app.Use(func(next buffalo.Handler) buffalo.Handler {
 			return func(c buffalo.Context) error {
-				c.Set("version", "0.9.5")
+				c.Set("version", "0.10.0")
 				c.Set("year", time.Now().Year())
 				c.Set("trainingURL", "http://www.gopherguides.com")
 				return next(c)

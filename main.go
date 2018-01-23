51ccd70d4dffdd2abc93421f1dcf3d7c704534bb
Mark Bates <mark@markbates.com>
2018-01-23 18:05:11 -0500

    print out the options

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"log"
 
 	"github.com/gobuffalo/gobuffalo/actions"
+	"github.com/kr/pretty"
 )
 
 func main() {
-	log.Fatal(actions.App().Serve())
+	a := actions.App()
+	pretty.Println("### a.Options ->", a.Options)
+	log.Fatal(a.Serve())
 }

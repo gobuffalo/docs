5a4c47e40a5ab74a0aab7c65706e633c7a548c0b
Mark Bates <mark@markbates.com>
2018-01-24 15:42:26 -0500

    only deploy on master

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -6,7 +6,7 @@ set -e # Abort script at first error
 set -u # Disallow unset variables
 
 # Only run when not part of a pull request and on the master branch
-if [ $TRAVIS_PULL_REQUEST != "false" -o $TRAVIS_BRANCH != "auto-deploy" ]
+if [ $TRAVIS_PULL_REQUEST != "false" -o $TRAVIS_BRANCH != "master" ]
 then
     echo "Skipping deployment on branch=$TRAVIS_BRANCH, PR=$TRAVIS_PULL_REQUEST"
     exit 0;

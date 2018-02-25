92cc7b21f8b334b834ef83f1aa2036e3ce93e2e9
Stanislas Michalak <stanislas.michalak@gmail.com>
2018-02-25 08:56:08 +0100

    Bump release version

--- actions/app.go
+++ actions/app.go
@@ -47,7 +47,7 @@ func App() *buffalo.App {
 
 		app.Use(func(next buffalo.Handler) buffalo.Handler {
 			return func(c buffalo.Context) error {
-				c.Set("version", "0.10.3")
+				c.Set("version", "0.11.0")
 				c.Set("goMinVersion", "1.8.1")
 				c.Set("year", time.Now().Year())
 				c.Set("trainingURL", "http://www.gopherguides.com")

e52d3d026280a02dc2534d86c9f612b06feb5220
Stanislas Michalak <stanislas.michalak@gmail.com>
2018-03-03 17:24:19 +0100

    Use the new "current_template" var to adapt improve btn with lang

--- actions/docs.go
+++ actions/docs.go
@@ -7,14 +7,14 @@ import (
 	"github.com/pkg/errors"
 )
 
-const docsRepoBase = "https://github.com/gobuffalo/gobuffalo/edit/master/templates"
+const docsRepoBase = "https://github.com/gobuffalo/gobuffalo/edit/master/templates/"
 
 // Docs serves documentation pages
 func Docs(c buffalo.Context) error {
 	for _, ext := range []string{"md", "html"} {
 		f := fmt.Sprintf("docs/%s.%s", c.Param("name"), ext)
 		if r.TemplatesBox.Has(f) {
-			c.Set("sourcePath", fmt.Sprintf("%s/%s", docsRepoBase, f))
+			c.Set("sourceRoot", docsRepoBase)
 			return c.Render(200, r.HTML(f, "docs-layout.html"))
 		}
 	}

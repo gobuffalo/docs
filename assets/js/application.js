b1df71153d792b24dedaa419f1d6e5885c26055b
Antonio Pagano <645522+paganotoni@users.noreply.github.com>
2018-09-18 00:29:08 -0500

    Adding minor fixes (video and fr features) (#330)
    * adding minor fixes (video and fr features)
    
    * minor js adjusting

--- assets/js/application.js
+++ assets/js/application.js
@@ -109,4 +109,11 @@ $(() => {
   $("#language").on("change", (e) => {
     $(e.target).closest("form").submit();
   });
+
+  $("body").on('hidden.bs.modal', function (e) {
+    var $iframes = $(e.target).find("iframe");
+    $iframes.each(function(index, iframe){
+      $(iframe).attr("src", $(iframe).attr("src"));
+    });
+  });  
 });

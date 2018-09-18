b3f13cb839df105e3a60062e766d6774d1b0c5d7
Antonio Pagano <apagano@wawand.co>
2018-09-17 20:22:17 -0500

    adding minor fixes (video and fr features)

--- assets/js/application.js
+++ assets/js/application.js
@@ -110,3 +110,12 @@ $(() => {
     $(e.target).closest("form").submit();
   });
 });
+
+$(function(){
+  $("body").on('hidden.bs.modal', function (e) {
+    var $iframes = $(e.target).find("iframe");
+    $iframes.each(function(index, iframe){
+      $(iframe).attr("src", $(iframe).attr("src"));
+    });
+  });
+});

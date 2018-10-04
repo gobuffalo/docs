83721317fce3186be19104fd9f29efd3c6efa540
Mark Bates <mark@markbates.com>
2018-10-04 17:40:59 -0400

    enables search indexing of godocs

--- actions/godoc.go
+++ actions/godoc.go
@@ -1,33 +1,92 @@
 package actions
 
 import (
+	"context"
 	"fmt"
-	"go/build"
-	"path/filepath"
+	"net/http"
+	"time"
 
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/envy"
+	gd "github.com/golang/gddo/doc"
 	"github.com/pkg/errors"
-	"golang.org/x/tools/godoc"
-	"golang.org/x/tools/godoc/vfs"
 )
 
-func indexGodocs() error {
+type githubTrans struct{}
 
-	fs := vfs.NewNameSpace()
-	c := build.Default
-	fmt.Println("### c.SrcDirs() ->", c.SrcDirs())
+func (githubTrans) RoundTrip(req *http.Request) (*http.Response, error) {
+	q := req.URL.Query()
+	q.Add("access_token", envy.Get("GITHUB_TOKEN", "x"))
+	req.URL.RawQuery = q.Encode()
+	return http.DefaultTransport.RoundTrip(req)
+}
 
-	root := vfs.OS(build.Default.GOROOT)
-
-	fs.Bind("/", root, "/", vfs.BindReplace)
-	fmt.Println("### build.Default.GOPATH ->", build.Default.GOPATH)
-	for _, p := range filepath.SplitList(build.Default.GOPATH) {
-		fmt.Println("### p ->", p)
-		fs.Bind("/src", vfs.OS(p), "/src", vfs.BindAfter)
+func indexGodocs(app *buffalo.App) error {
+	list := []string{
+		"github.com/gobuffalo/buffalo",
+		"github.com/gobuffalo/pop",
+		"github.com/gobuffalo/fizz",
 	}
-	corpus := godoc.NewCorpus(fs)
-	corpus.Verbose = true
-	if err := corpus.Init(); err != nil {
+	for _, l := range list {
+		if err := indexGodoc(l, app); err != nil {
+			fmt.Println("### err ->", err)
+		}
+	}
+	return nil
+}
+
+func indexGodoc(pkg string, app *buffalo.App) error {
+	ctx, cancel := context.WithTimeout(app.Context, 5*time.Second)
+	defer cancel()
+
+	c := &http.Client{
+		Transport: githubTrans{},
+	}
+	dpkg, err := gd.Get(ctx, c, pkg, "")
+	if err != nil {
 		return errors.WithStack(err)
 	}
+	for _, sd := range dpkg.Subdirectories {
+		sd = pkg + "/" + sd
+		go func(sd string, app *buffalo.App) {
+			err := indexGodoc(sd, app)
+			if err != nil {
+				fmt.Println(sd, err)
+			}
+		}(sd, app)
+	}
+
+	ub := func(name string) string {
+		return "https://godoc.org/" + dpkg.ImportPath + "#" + name
+	}
+
+	for _, t := range dpkg.Funcs {
+		d := doc{
+			URL:  ub(t.Name),
+			Body: t.Doc,
+		}
+		if err := index.Index(d.URL, d); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	for _, t := range dpkg.Types {
+		d := doc{
+			URL:  ub(t.Name),
+			Body: t.Doc,
+		}
+		if err := index.Index(d.URL, d); err != nil {
+			return errors.WithStack(err)
+		}
+		for _, m := range t.Methods {
+			d := doc{
+				URL:  ub(t.Name + "." + m.Name),
+				Body: m.Doc,
+			}
+			if err := index.Index(d.URL, d); err != nil {
+				return errors.WithStack(err)
+			}
+		}
+	}
+
 	return nil
 }
--- actions/godoc_test.go
+++ actions/godoc_test.go
@@ -1,13 +1,1 @@
 package actions
-
-import (
-	"testing"
-
-	"github.com/stretchr/testify/require"
-)
-
-func Test_indexGodocs(t *testing.T) {
-	r := require.New(t)
-	r.NoError(indexGodocs())
-	r.False(true)
-}
